9f509a8b7460f1dd670b1978e6574611300e7508
Kevin <kevin.yu.kfy@gmail.com>
2021-06-23 22:43:35 +0000

    Add 1_queries

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -11,7 +11,7 @@ VALUES('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvm
 
 -- fake data for properties table
 INSERT INTO properties(owner_id, title , description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-(1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
+VALUES(1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
 (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 852.34, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
 (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 460.58, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', 44583, true),
 (4, 'Headed know', 'description', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg', 826.40, 0, 5, 5, 'Canada', '513 Powov Grove', 'Jaebvap', 'Ontario', 38051, true),
@@ -30,8 +30,7 @@ VALUES('2018-09-11', '2018-09-26', 2 ,3),
 
 -- fake data for property_reviews table
 INSERT INTO property_reviews(guest_id, property_id, reservation_id, rating, message)
-VALUES(2, 5, 10, 3, 'message'),
-(1, 4, 1 , 4, 'message'),
-(8, 1, 2 , 4, 'message'),
-(3, 8, 5 , 4, 'message'),
-(4, 2, 7 , 4, 'message');
+VALUES(2, 2, 1019998, 3, 'message'),
+(1, 4, 1019997, 4, 'message'),
+(8, 1, 1019998, 4, 'message'),
+(3, 8, 1019998, 4, 'message');
